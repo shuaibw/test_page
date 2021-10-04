6df7cf60c38fa57cee6b6d6f126626a1e3d4ca1c
shuaibw <1805010@ugrad.cse.buet.ac.bd>
2021-10-04 11:47:15 +0600

    Add js from unpkg

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-import * as THREE from 'https://threejs.org/build/three.js';
-import { OrbitControls } from 'https://rawgit.com/mrdoob/three.js/dev/examples/jsm/controls/OrbitControls.js';
+import {OrbitControls} from 'https://unpkg.com/three@0.127.0/examples/jsm/controls/OrbitControls.js'
+import * as THREE from 'https://unpkg.com/three@0.127.0/build/three.module.js';
 
 // Setup
 
